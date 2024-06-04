e7bda0c32e0953998984901ba4121e5defcedf64
Ján Rusnák <jr@i-tel.sk>
2024-06-04 17:35:10 +0200

    Whitespaces changed.

--- prj/src/main_tinsy.c
+++ prj/src/main_tinsy.c
@@ -323,16 +323,16 @@ static void led_ctl_cmd(int led_id, const char *cmd)
 
 	msg(INF, cmd_accp);
 	switch (led_id) {
-	case 1  :
+	case 1 :
 		ld = LEDUI1;
 		break;
-	case 2  :
+	case 2 :
 		ld = LEDUI2;
 		break;
-	case 3  :
+	case 3 :
 		ld = LEDUI3;
 		break;
-	case 4  :
+	case 4 :
 		ld = LEDUI4;
 		break;
 	default :
@@ -361,7 +361,7 @@ static void rgb_ctl_cmd(int led_id, const char *cmd)
 {
 	msg(INF, cmd_accp);
 	switch (led_id) {
-	case 1  :
+	case 1 :
 		if (0 == strcmp(cmd, "on")) {
 			set_pin_lev(RGB_R_IO_PIN, RGB_R_IO_CONT, LOW);
 		} else if (0 == strcmp(cmd, "off")) {
@@ -370,7 +370,7 @@ static void rgb_ctl_cmd(int led_id, const char *cmd)
 			msg(INF, "error: bad cmd\n");
 		}
 		break;
-	case 2  :
+	case 2 :
 		if (0 == strcmp(cmd, "on")) {
 			set_pin_lev(RGB_G_IO_PIN, RGB_G_IO_CONT, LOW);
 		} else if (0 == strcmp(cmd, "off")) {
@@ -379,7 +379,7 @@ static void rgb_ctl_cmd(int led_id, const char *cmd)
 			msg(INF, "error: bad cmd\n");
 		}
 		break;
-	case 3  :
+	case 3 :
 		if (0 == strcmp(cmd, "on")) {
 			set_pin_lev(RGB_B_IO_PIN, RGB_B_IO_CONT, LOW);
 		} else if (0 == strcmp(cmd, "off")) {
